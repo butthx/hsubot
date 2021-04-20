4885f9beccdae024eaa4d95439853b484de1cc22
butthx <tanya.duwi23@gmail.com>
2021-04-20 13:41:34 +0700

    fixed heroku

--- main.js
+++ main.js
@@ -2,24 +2,19 @@ const fs = require("fs")
 // unuk mencegah error cek apakah file config.js ada
 if(!fs.existsSync("./config.js")){
   let dataFs = `module.exports = {
-  // di dapat dari https://my.telegram.org/
   API_ID: ${Number(process.env.api_id)},
-  API_HASH: ${String(process.env.api_hash)},
+  API_HASH: "${String(process.env.api_hash)}",
   pathTDLib : './tdlib/libtdjson.so',
-  // aktifkan jika pakai bot API
   BOT_API: ${Boolean(String(process.env.bot_api))},
-  // HANYA jika BOT_API true, token bot API dari @botfather
-  BOT_TOKEN: ${String(process.env.bot_token)},
-  // untuk verbose mode
+  BOT_TOKEN: "${String(process.env.bot_token)}"",
   debug: {
     active: ${Boolean(String(process.env.debug_mode))},
-    level: ${Number(process.env.debug_level)|1} // 1 event only, 2 detail, 3 semua termasuk object dan fungsi
+    level: ${Number(process.env.debug_level)||1}
   },
   admin:
   {
     active: ${Boolean(String(process.env.admin_mode))},
-    // Jika admin.active, sesuaikan dengan ID mu
-    id: ${[Number(process.env.admin_id)|"OWNER_ID"]},
+    id: ${[Number(process.env.admin_id)||"OWNER_ID"]},
   },
 }`
   fs.writeFileSync("./config.js",dataFs)
