0229750d577e8fc8a98221b495649b3a0bdee899
butthx <tanya.duwi23@gmail.com>
2021-04-20 13:35:10 +0700

    fixed heroku

--- main.js
+++ main.js
@@ -7,17 +7,17 @@ if(!fs.existsSync("./config.js")){
   API_HASH: ${String(process.env.api_hash)},
   pathTDLib : ${'./tdlib/libtdjson.so'},
   // aktifkan jika pakai bot API
-  BOT_API: ${toBool(String(process.env.bot_api))},
+  BOT_API: ${Boolean(String(process.env.bot_api))},
   // HANYA jika BOT_API true, token bot API dari @botfather
   BOT_TOKEN: ${String(process.env.bot_token)},
   // untuk verbose mode
   debug: {
-    active: ${toBool(String(process.env.debug_mode))},
+    active: ${Boolean(String(process.env.debug_mode))},
     level: ${Number(process.env.debug_level)|1} // 1 event only, 2 detail, 3 semua termasuk object dan fungsi
   },
   admin:
   {
-    active: ${toBool(String(process.env.admin_mode))},
+    active: ${Boolean(String(process.env.admin_mode))},
     // Jika admin.active, sesuaikan dengan ID mu
     id: ${[Number(process.env.admin_id)|"OWNER_ID"]},
   },
